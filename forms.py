cd35d1dcab00e5b095e3ff47e4b46337a332c5e6
Andrey Pachay <apachay@octoberry.ru>
2014-05-31 21:41:25 +0400

    tpl

--- forms.py
+++ forms.py
@@ -10,4 +10,4 @@ class CreateEventForm(Form):
     card = StringField(u'Номер карты:', [validators.DataRequired(message=u'Укажите номер карты'),
                                          validators.Regexp(regex=r'^\d{16}$',
                                                            message=u'Укажите номер карты в формате '
-                                                                   u'XXXX-XXXX-XXXX-XXXX')])
+                                                                   u'XXXXXXXXXXXXXXXX')])
