e0eb93945c450b024df3c369e8207ba89e5760d2
Eugeny Chuvelev <e.chuvelev@gmail.ru>
2014-06-01 03:41:43 +0400

    Fix submit form bug

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,4 @@
-function replaceMask($cardField){
-    $cardField.val($cardField.val().replace(/-/g,''));
-}
-
-(function(){
-
-    $(function(){
-
-        var $inputCard = $('#present\\[cardnumber\\]');
-
-            $inputCard.mask('0000-0000-0000-0000');
-
-            $('form[role="presentSubmit"]').on('submit', function(){
-                replaceMask($inputCard);
-            });
-    });
-
-})();
+$(function(){
+    $('#present\\[cardnumber\\]').mask('0000-0000-0000-0000');
+    $('#card_source').mask('0000-0000-0000-0000');
+});
