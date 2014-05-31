16a7e1c7a01378be417798d4b7d33d9689cbd03f
Andrey Pachay <apachay@octoberry.ru>
2014-05-31 19:07:05 +0400

    event fields

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from forms import CreateEventForm
-from pony.orm import db_session, commit, Database, Required
+from pony.orm import db_session, Database, Required, commit
 from datetime import datetime
 
 app = Flask(__name__)
@@ -18,6 +18,8 @@ class Events(db.Entity):
     image = Required(unicode)
     updated_at = Required(datetime)
     created_at = Required(datetime)
+
+
 db.generate_mapping()
 
 
@@ -26,19 +28,24 @@ db.generate_mapping()
 def create():
     form = CreateEventForm(request.form)
     if request.method == 'POST' and form.validate():
-        Events(description=form.description.data,
-               amount=form.amount.data,
-               card=form.card.data,
-               image=form.image.data,
-               created_at=datetime.now(),
-               updated_at=datetime.now())
-        return redirect(url_for('success'))
+        event = Events(description=form.description.data,
+                       amount=form.amount.data,
+                       card=form.card.data,
+                       image=form.image.data,
+                       created_at=datetime.now(),
+                       updated_at=datetime.now())
+        commit()
+        return redirect(url_for('event', event_id=event.id))
     return render_template('create.html', form=form)
 
 
-@app.route('/success', methods=['GET'])
-def success():
-    return render_template('success.html')
+@app.route('/event/<int:event_id>')
+@db_session
+def event(event_id):
+    event = Events.get(id=event_id)
+    event.url = url_for('event', event_id=event_id, _external=True)
+    return render_template('event.html', event=event)
+
 
 if __name__ == "__main__":
     app.run()
