d2975a4c09e1b1eebd887a0b43ad7578cf64d9c5
Andrey Zakharov <fuse5@yandex.ru>
2014-08-04 14:22:25 +0400

    fab update

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-from fabric.api import env, cd, run
+from fabric.api import env, cd, run, local
 
 USERNAME = 'fuse'
 SERVER = 'octoberry.ru'
@@ -11,6 +11,9 @@ env.hosts = ["%s@%s" % (USERNAME, SERVER)]
 
 
 def deploy():
+    """
+    Remote deploy
+    """
     with cd(PROJECT_DIR):
         run('sudo git pull')
         run('sudo pyclean .')
@@ -19,3 +22,16 @@ def deploy():
         run('sudo %s/bin/yoyo-migrate -b apply migrations/' % VENV_DIR)
 
     run('sudo service uwsgi restart')
+
+
+def update():
+    """
+    Local update on server
+    """
+    local('sudo git pull')
+    local('sudo pyclean .')
+
+    local('sudo %s/bin/pip install -q -r requirements.txt' % VENV_DIR)
+    local('sudo %s/bin/yoyo-migrate -b apply migrations/' % VENV_DIR)
+
+    local('sudo service uwsgi restart')
